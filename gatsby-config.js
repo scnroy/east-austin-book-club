3ab05a2c104dfb627c556af7cdd54edb205b704f
sconroy <s.cnroy@gmail.com>
2019-09-27 16:00:29 -0700

    update config

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,14 @@ module.exports = {
             resolve: `gatsby-source-filesystem`,
             options: {
                 name: `images`,
-                path: `${__dirname}/src/images`,
+                path: `${__dirname}/static/assets`,
+            },
+        },
+        {
+            resolve: `gatsby-source-filesystem`,
+            options: {
+                name: `content`,
+                path: `${__dirname}/content`,
             },
         },
         {
@@ -21,18 +28,19 @@ module.exports = {
         `gatsby-plugin-theme-ui`,
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-                name: `gatsby-starter-default`,
-                short_name: `starter`,
-                start_url: `/`,
-                background_color: `#663399`,
-                theme_color: `#663399`,
-                display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-            },
-        },
+        `gatsby-transformer-remark`,
+        // {
+        //     resolve: `gatsby-plugin-manifest`,
+        //     options: {
+        //         name: `gatsby-starter-default`,
+        //         short_name: `starter`,
+        //         start_url: `/`,
+        //         background_color: `#663399`,
+        //         theme_color: `#663399`,
+        //         display: `minimal-ui`,
+        //         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        //     },
+        // },
         // this (optional) plugin enables Progressive Web App + Offline functionality
         // To learn more, visit: https://gatsby.dev/offline
         // `gatsby-plugin-offline`,
