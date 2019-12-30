14b9460b0fb47013f534be0892374699cc2e446f
sconroy <s.cnroy@gmail.com>
2019-12-29 18:53:18 -0600

    add space, small refactor

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,47 @@ import SEO from '../components/seo'
 import {btn} from '../components/button'
 import {montserrat} from '../styles/vars'
 
+const Book = ({slug, title, author, date, description, link}) => (
+    <li
+        css={css`
+            margin-bottom: 3rem;
+        `}
+    >
+        <h3
+            css={css`
+                margin-bottom: 0.5rem;
+            `}
+        >
+            <Link to={slug}>{title}</Link> by {author}
+        </h3>
+        <p
+            css={css`
+                ${montserrat}
+            `}
+        >
+            {date}
+        </p>
+        <p
+            css={css`
+                padding-left: 1em;
+                border-left: 2px solid black;
+            `}
+        >
+            {description}
+            <br />
+            <a
+                href={link}
+                css={css`
+                    margin-top: 1rem;
+                    ${btn}
+                `}
+            >
+                RSVP ⇾
+            </a>
+        </p>
+    </li>
+)
+
 const IndexPage = ({
     path,
     data: {
@@ -21,41 +62,7 @@ const IndexPage = ({
             `}
         >
             {edges.map(({node: {frontmatter, fields}}) => (
-                <li>
-                    <h3
-                        css={css`
-                            margin-bottom: 0.5rem;
-                        `}
-                    >
-                        <Link to={fields.slug}>{frontmatter.title}</Link> by{' '}
-                        {frontmatter.author}
-                    </h3>
-                    <p
-                        css={css`
-                            ${montserrat}
-                        `}
-                    >
-                        {frontmatter.date}
-                    </p>
-                    <p
-                        css={css`
-                            padding-left: 1em;
-                            border-left: 2px solid black;
-                        `}
-                    >
-                        {frontmatter.description}
-                        <br />
-                        <a
-                            href={frontmatter.link}
-                            css={css`
-                                margin-top: 1rem;
-                                ${btn}
-                            `}
-                        >
-                            RSVP ⇾
-                        </a>
-                    </p>
-                </li>
+                <Book {...frontmatter} {...fields} key={fields.slug} />
             ))}
         </ul>
     </Layout>
